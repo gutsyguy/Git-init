56153a4a9803c8952711058286440f1528511d38
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-06 21:45:05 -0800

    NavBar+GoogleAuth

--- web_git_init/src/App.js
+++ web_git_init/src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import NavBarIndex from './components/Navbar';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+  } from "react-router-dom";
+import About from './pages/about';
+import Home from './pages';
+import ConnectWithUs from './pages/communication/connectwithus';
+import Blog from './pages/communication/blog';
+import Profile from './pages/profile';
+import SignIn from './pages/signin';
+import Protected from './Protected';
+import MentalHealthInfo from './pages/mentalHealthInfo';
+import Support from './pages/support';
+
+
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+    <NavBarIndex/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/communication/connectwithus' element={<ConnectWithUs/>} />
+          <Route path='/mentalHealthInfo' element={<MentalHealthInfo/>} />
+          <Route path='/communication/blog' element={<Blog/>} />
+          <Route path='/profile' element={<Protected isAuth={isAuth}><Profile/></Protected>} />
+          <Route path='/signin' element={<SignIn setIsAuth={setIsAuth}/>} />
+          <Route path='/support' element={<Support/>} />
+          {/* <Route path="/createPost" element={<Protected isAuth={isAuth}><CreatePost/></Protected>} /> */}
+        </Routes>
+      </Router>
     </div>
+    
   );
 }
 
